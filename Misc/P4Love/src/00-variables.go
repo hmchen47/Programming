27090244c07317ea37c25f81569d8a898887d2e6
H.-M. Fred Chen <h.m.chen@ieee.org>
2019-10-22 22:09:58 -0700

    feat: declare variable w/o assigning value in Go

--- Misc/P4Love/src/00-variables.go
+++ Misc/P4Love/src/00-variables.go
@@ -24,7 +24,8 @@ func main() {
 
 	// change the value of a
 	a = a + 14
-	fmt.Println(a)
+	fmt.Println(a) // result: 42
+	fmt.Println()
 
 	// declaraction examples
 	var x float64 = -2.6
@@ -32,6 +33,15 @@ func main() {
 	var hello string = "Hello!" // using double quotes to indicate a string
 	var statement bool = true
 
-	fmt.Println(x, sym, hello, statement)
+	fmt.Println(x, sym, hello, statement) // result: -2.6 67 Hello! true
+	fmt.Println()
+
+	// omitting values of variables
+	var x1 float64
+	var sym1 byte
+	var hello1 string
+	var statement1 bool
+
+	fmt.Println(x1, sym1, hello1, statement1) // result: 0 0  false
 
 }
