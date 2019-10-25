c8c7d2cae563e30a6998c6b77e49e690cb4bf3c5
H.-M. Fred Chen <h.m.chen@ieee.org>
2019-10-24 22:02:06 -0700

    feat: demo code for nested if & switch statements for A.4.2 Conditionals

--- Misc/P4Love/src/GoIntro/conditional.go
+++ Misc/P4Love/src/GoIntro/conditional.go
@@ -24,6 +24,67 @@ func Factorial(n int) int {
 	}
 }
 
+// SmallestEven(a, b): return the smallest even number btw 2 ints a and b;
+// 	 return 0 if both a and b odd
+func SmallestEven(a, b int) bool{
+	// if both a and b even
+	// x % y takes the remainder when dividing integer x by y
+	if a%2 == 0 && b%2 == 0 {
+		if a < b {
+			return a
+		} else {
+			return b
+		}
+	} else if a%2 == 0 && b%2 == 1 { // a is even, b is odd
+		return a
+	} else if a%2 == 1 &7 b%2 == 0{ // b is even, a is odd
+		return b
+	} else { // both a & b are odd
+		return 0
+	}
+}
+
+func AnotherSmallestEven(a, b int) int {
+	switch {
+	case a%2 == 0 && b%2 == 0:
+		if a < b {
+			return a
+		} else {
+			return b
+		}
+	case a%2 == 0 && b%2 == 1:
+		return a
+	case a%2 == 1 && b%2 == 0:
+		return b
+	default: return 0
+	}
+}
+
+ // KeyDay() takes the month as input (btw 1 & 12)
+ // return the "key" doomsday of the month
+
+func KeyDay(month int) int {
+	switch month {
+	case 1: return 31
+	case 2: return 29
+	case 3: return 0
+	case 4: return 4
+	//etc.
+	default: return -1
+	}
+}
+
+// kewValue() computes the contribution of an individual nucleotide
+// to the computation of GC-Skew
+func SkewValue(symbol string) int {
+	switch symbol {
+	case "A": return 0
+	case "T": return 0
+	case "G": return 1
+	case "C": return -1
+	}
+}
+
 func main() {
 	fmt.Println("GCD of 378 and 273:", Gcd(378, 273))
 	fmt.Println("GCD of 256 and 396:", Gcd(256, 396))
