1bdec7bebb572a4288151ed5944ef1ee3a8dcc61
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-09-01 22:31:46 -0700

    style: reformat the code

--- Python/Scraping/WebScraping-Udemy/src/04-sportsPrj.py
+++ Python/Scraping/WebScraping-Udemy/src/04-sportsPrj.py
@@ -14,9 +14,6 @@ Scraping 2012 Premier League Data
 URL: https://www.skysports.com/premier-league-table/2011
 """
 
-import requests
-
-
 def download_pg(url):
 
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
