0be5b5a382a5ab06178bf5c9b621d5c433edab43
H.-M. Fred Chen <h.m.chen@ieee.org>
2021-01-23 12:54:13 -0800

    feat: add demo files for 1.6.3 Modifying an HTML document

---
+++ WebDev/Frontend-W3C/5-JavaScript/src/js/01f-example05.js
@@ -0,0 +1,22 @@
+function buildTable() {
+  addLineToHTMLTable("Michel", "Buffa");
+    addLineToHTMLTable("Marie-Claire", "Forgue");
+    addLineToHTMLTable("Tim", "Berners-Lee");
+}
+
+// Add a line to the HTML table
+function addLineToHTMLTable(firstName, lastName) {
+  // Get the body of the table using the selector API
+  var tableBody = document.querySelector("#tableContactBody");
+
+// Add a new row at the end of the table
+  var newRow   = tableBody.insertRow();
+
+ // add  new cells to the row
+ var firstNameCell  = newRow.insertCell();
+ firstNameCell.innerHTML = firstName;
+
+ var lastNameCell   = newRow.insertCell();
+ lastNameCell.innerHTML = lastName;
+}
+
