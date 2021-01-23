756aef0653b2be2ba33384dcbdeeba45ea0387e7
H.-M. Fred Chen <h.m.chen@ieee.org>
2021-01-23 13:20:00 -0800

    feat: chg demo files for 1.6.4 Modifying CSS styles on the fly

--- WebDev/Frontend-W3C/5-JavaScript/src/js/01f-example05.js
+++ WebDev/Frontend-W3C/5-JavaScript/src/js/01f-example05.js
@@ -9,14 +9,14 @@ function addLineToHTMLTable(firstName, lastName) {
   // Get the body of the table using the selector API
   var tableBody = document.querySelector("#tableContactBody");
 
-// Add a new row at the end of the table
+  // Add a new row at the end of the table
   var newRow   = tableBody.insertRow();
 
- // add  new cells to the row
- var firstNameCell  = newRow.insertCell();
- firstNameCell.innerHTML = firstName;
+  // add  new cells to the row
+  var firstNameCell  = newRow.insertCell();
+  firstNameCell.innerHTML = firstName;
 
- var lastNameCell   = newRow.insertCell();
- lastNameCell.innerHTML = lastName;
+  var lastNameCell   = newRow.insertCell();
+  lastNameCell.innerHTML = lastName;
 }
 
