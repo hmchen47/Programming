2ec39d0099c164b14ed08fdd3bd67b74dc47a657
H.-M. Fred Chen <h.m.chen@ieee.org>
2021-01-27 21:19:11 -0800

    feat: add demo file for 2.2.1 Boolean values and logical operators

---
+++ WebDev/Frontend-W3C/5-JavaScript/src/js/02b-example01.js
@@ -0,0 +1,33 @@
+var javaScriptCourse = true;
+[Local Demo]
+if(javaScriptCourse) {
+   console.log("This is a JavaScript course!");
+} else {
+  console.log("This is NOT a JavaScript course!"); 
+}
+
+// comparison operators
+// < > <= >= == !=, and === !==
+// comparison operators: < > <= >= == !=
+ 
+var age = "72"; 
+
+if(age < 2) {
+  console.log("I'm a baby");
+} else if(age < 18) { 
+  console.log("I'm a child");
+} else if (age < 60) {
+  console.log("I'm an adult");
+} else {
+  // age >= 60
+  console.log("I'm old");
+}
+
+if(age === 72) { 
+  console.log("I'm 72!");
+}
+
+if((age > 12) || (age < 14)) {
+  console.log("I'm a young teenager!");
+}
+
