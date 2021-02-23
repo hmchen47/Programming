eafec6cb1b5b4a4439a33b8bc35204c9753861c9
H.-M. Fred Chen <h.m.chen@ieee.org>
2021-02-22 22:44:35 -0800

    feat: add demo files for 4.2.7 Adding/deleting properties and methods

---
+++ WebDev/Frontend-W3C/5-JavaScript/src/js/04b-example07.js
@@ -0,0 +1,23 @@
+// empty object with no properties/methods
+var darkVador = {};
+
+// add properties after darkVador has been created
+ darkVador.race = 'human';
+  darkVador.job = 'villain';
+  darkVador.talk = function() {
+    return 'come to the dark side, Luke!' + this.breathe();
+  };
+
+  darkVador.describeYourself = function() {
+    return "I'm a " + this.race + " and I'm a " + this.job + " in a series of movies!" + this.breathe();
+  };
+
+  darkVador.breathe = function() {
+    return ".....shhhhhhhhh.....";
+  };
+
+
+function dvSpeak() {
+ document.body.innerHTML += '<p>Dark Vador describes himself: ' + darkVador.describeYourself(); + '</p>';  document.body.innerHTML += '<p>Dark Vador says: ' + darkVador.talk(); + '</p>';
+}
+
