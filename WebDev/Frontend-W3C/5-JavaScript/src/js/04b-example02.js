1ef1a74b0f2051122d793f8a8f568835d61b3b61
H.-M. Fred Chen <h.m.chen@ieee.org>
2021-02-22 20:25:43 -0800

    feat: add and chg demo files for 4.2.5 Elements, properties and methods

--- WebDev/Frontend-W3C/5-JavaScript/src/js/04b-example02.js
+++ WebDev/Frontend-W3C/5-JavaScript/src/js/04b-example02.js
@@ -1,15 +1,32 @@
-var darkVador = {
-  race: 'human',
-  job: 'villain',
-  talk: function() {
-    return 'come to the dark side, Luke!';
-  },
-  describeYourself: function() {
-    return "I'm a " + this.race + " and I'm a " + this.job + " in a series of movies!";
-  }
-}
+// empty object with properties/methods
+var darkVador = {};
+
+// add properties after darkVador has been created
+ darkVador.race = 'human';
+ darkVador.job = 'villain';
+
+// add some methods
+  darkVador.talk = function() {
+    return 'come to the dark side, Luke!' + this.breathe();
+  };
+
+  darkVador.describeYourself = function() {
+    return "I'm a " + this.race 
+      + " and I'm a " + this.job 
+      + " in a series of movies!" + this.breathe();
+  };
+
+  darkVador.breathe = function() {
+    return ".....shhhhhhhhh.....";
+  };
+
 
 function dvSpeak() {
- document.body.innerHTML += '<p>Dark Vador describes himself: ' + darkVador.describeYourself(); + '</p>';  document.body.innerHTML += '<p>Dark Vador says ' + darkVador.talk(); + '</p>';
+ document.body.innerHTML += '<p>Dark Vador describes himself: ' + darkVador.describeYourself(); + '</p>';  document.body.innerHTML += '<p>Dark Vador says: ' + darkVador.talk(); + '</p>';
+}
+
+function deleteSomeProperties() {
+  delete darkVador.race;
+  delete darkVador.job;
 }
 
--- WebDev/Frontend-W3C/5-JavaScript/src/js/04b-example03.js
+++ WebDev/Frontend-W3C/5-JavaScript/src/js/04b-example03.js
@@ -1,80 +1,16 @@
-// useful to have them as global variables
-var canvas, ctx, w, h; 
-var mousePos;
-
-var player = {
-  x:10,
-  y:10,
-  width:20,
-  height:20,
-  color:'red'
-}
-
-window.onload = function init() {
-    // called AFTER the page has been loaded
-    canvas = document.querySelector("#myCanvas");
-  
-    // often useful
-    w = canvas.width; 
-    h = canvas.height;  
-  
-    // important, we will draw with this object
-    ctx = canvas.getContext('2d');
-  
-    // add a mousemove event listener to the canvas
-    canvas.addEventListener('mousemove', mouseMoved);
-
-    // ready to go !
-    mainLoop();
-};
-
-function mouseMoved(evt) {
-    mousePos = getMousePos(canvas, evt);
-}
-
-function getMousePos(canvas, evt) {
-    // necessary work in the canvas coordinate system
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: evt.clientX - rect.left,
-        y: evt.clientY - rect.top
-    };
-}
-
-function movePlayerWithMouse() {
-  if(mousePos !== undefined) {
-    player.x = mousePos.x;
-    player.y = mousePos.y;
+var darkVador = {
+  race: 'human',
+  job: 'villain',
+  talk: function() {
+    return 'come to the dark side, Luke!';
+  },
+  describeYourself: function() {
+    return "I'm a " + this.race + " and I'm a " + this.job + " in a series of movies!";
   }
 }
 
-function mainLoop() {
-  // 1 - clear the canvas
-  ctx.clearRect(0, 0, w, h);
-  
-  // draw the player
-  drawFilledRectangle(player);
-  
-  // make the player follow the mouse
-  movePlayerWithMouse();
-  
-  // ask for a new animation frame
-  requestAnimationFrame(mainLoop);
-}
-
-function drawFilledRectangle(r) {
-    // GOOD practice: save the context, use 2D trasnformations
-    ctx.save();
-  
-    // translate the coordinate system, draw relative to it
-    ctx.translate(r.x, r.y);
-  
-    ctx.fillStyle = r.color;
-    // (0, 0) is the top left corner of the monster.
-    ctx.fillRect(0, 0, r.width, r.height);
-  
-    // GOOD practice: restore the context
-    ctx.restore();
+function dvSpeak() {
+ document.body.innerHTML += '<p>Dark Vador describes himself: ' + darkVador.describeYourself(); + '</p>';  document.body.innerHTML += '<p>Dark Vador says ' + darkVador.talk(); + '</p>';
 }
 
 
