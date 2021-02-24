e8f9422fe912b0477bd40eb3a0f6c88d73b49bf7
H.-M. Fred Chen <h.m.chen@ieee.org>
2021-02-23 16:14:17 -0800

    feat: add demo files for 4.3.3 Creating objects using modern JavaScript's classes

--- WebDev/Frontend-W3C/5-JavaScript/src/js/04c-example03.js
+++ WebDev/Frontend-W3C/5-JavaScript/src/js/04c-example03.js
@@ -1,8 +1,32 @@
-function Hero(name, side) {
+/* ES5's constructor function syntax is not easy to read. If someone does not respect the "conventions" that we've just discussed (start the class with an uppercase, etc.), then the code may work, but it will be difficult to guess that we are not in front of a regular function.
+
+ES5 construction function
+*/
+function HeroES5(name, side) {
   this.name = name;
   this.side = side;
   
-  this.speak = function() {
+  this.describeYourself = function() {
+    console.log("I'm " + this.name + " and I'm from the " + this.side);
+  }
+}
+
+let ianSoloES5 = new HeroES5('Ian Solo', 'Rebels');
+
+
+/* ES6 created a class keyword and a constructor keyword, along with advanced concepts that will be the subject of an "advanced JavaScript" course. 
+
+Using ES6 classes
+*/
+class Hero {
+  // must be UNIQUE!, called when "new" is used
+  constructor(name, side) { 
+    this.name = name;
+    this.side = side;   
+  }
+  
+  // no "function" keyword here!
+  speak() {
     return "<p>My name is " + this.name +
       ", I'm with the " + this.side + ".</p>";
   }
@@ -17,4 +41,5 @@ function makeHeroesSpeak() {
    document.body.innerHTML += luke.speak();
    document.body.innerHTML += ianSolo.speak();
 }
+
 
